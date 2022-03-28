e21a41c80f8911bacfe1d24620f58725bd2a0603
Rocio Garrido <rocio.garrido@wefox.com>
2022-03-28 09:47:58 +0200

    Getters and setters

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -11,7 +11,16 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = 'carts';
+
+  private _listFilter: string = '';
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    console.log('In setter:', value);
+  }
+
   products: IProduct[] = [
     {
       "productId": 2,
@@ -40,6 +49,6 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log('In OnInit');
+    this._listFilter = 'cart';
   }
 }
